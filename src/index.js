0ced9f421545e296d47a374f64584817b14ba4c8
d.voyanets <diana.voyanets@gmail.com>
2018-02-20 16:13:52 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+        var count = 0;
+    
+        preferences.forEach((item, i, arr) => {
+            var left = arr[i] - 1, 
+                mid = arr[left] - 1,
+                right = arr[mid] - 1;
+            
+            if (right == i && mid != right && left != right) {
+                count++;
+                
+                arr[left] = null;
+                arr[mid] = null;
+                arr[right] = null;
+            }
+        });
+    
+        return count;
+    };
+
